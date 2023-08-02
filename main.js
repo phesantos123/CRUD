6522a546d8a58dc0b202a931f784e655740a7b31
Pedro Santos <phsantos021@gmail.com>
2023-08-02 00:38:52 -0300

    mock

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () =>
   document.getElementById('modal').classList.remove('active')
 
+const temClient = {
+  nome: 'Pedro',
+  email: 'ph.santos021@gmail.com',
+  phone: '11-123456789',
+  city: 'Diadema'
+}
+
+const createClient = client => {
+  const db_client = JSON.parse(localStorage.getItem('db_client')) ?? []
+  db_client.push(client)
+  localStorage.setItem('db_client', JSON.stringify(db_client))
+}
+
+//eventos
 document.getElementById('cadastrarCliente').addEventListener('click', openModal)
 
 document.getElementById('modalClose').addEventListener('click', closeModal)
